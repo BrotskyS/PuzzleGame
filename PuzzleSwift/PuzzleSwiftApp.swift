a0b1c64a1b617f5d742f9ab5df34450515715439
BrotskyS <sergiypik.work@gmail.com>
2022-06-26 17:51:44 +0300

    Initial Commit

---
+++ PuzzleSwift/PuzzleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleSwiftApp.swift
+//  PuzzleSwift
+//
+//  Created by Sergiy Brotsky on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
